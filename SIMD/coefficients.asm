30a8f665131be49e7130a766b710bfaaeb09785b
Colin McKinney <mckinnec@wabash.edu>
2022-11-29 11:35:33 -0500

    Added coefficient file

---
+++ SIMD/coefficients.asm
@@ -0,0 +1,57 @@
+
+//32 bit Apollo Coefficients
+ctabApollo32:
+  .word 0x3fc90a4d
+  .word 0xbf24aaab
+  .word 0x3d94e916
+
+//64 bit Apollo Coefficients
+ctabApollo64:
+  .dword 0x3ff92149914389c3
+  .dword 0xbfe49555673aa1bc
+  .dword 0x3fb29d22bb15eb24
+
+//32 bit Chebyshev Coefficients
+ctabCheby32:
+  .word 0x3fc90fdb
+  .word 0xbf255de7
+  .word 0x3da335e3
+  .word 0xbb996966
+  .word 0x39283c19
+  .word 0xb6718319
+  .word 0x33744fb8
+  .word 0xb031481f
+
+//64 bit Chebyshev Coefficients
+ctabCheby64:
+  .dword 0x3ff921fb54442d17
+  .dword 0xbfe4abbce625bd83
+  .dword 0x3fb466bc677522bd
+  .dword 0xbf732d2cce1ea145
+  .dword 0x3f25078327046959
+  .dword 0xbece30631bdf732d
+  .dword 0x3e6e89f6fe44fe7b
+  .dword 0xbe062903d02bb153
+
+
+//32 bit Taylor Coefficients
+ctabTaylor32:
+  .word 0x3fc90fdb
+  .word 0xbf255de7
+  .word 0x3da335e3
+  .word 0xbb996966
+  .word 0x39283c1a
+  .word 0xb67183a8
+  .word 0x33747a1a
+  .word 0xb037d6dd
+
+//64 bit Taylor Coefficients
+ctabTaylor64:
+  .dword 0x3ff921fb54442d18
+  .dword 0xbfe4abbce625be53
+  .dword 0x3fb466bc6775aae2
+  .dword 0xbf732d2cce62bd86
+  .dword 0x3f250783487ee782
+  .dword 0xbece3074fde8871f
+  .dword 0x3e6e8f434d018d63
+  .dword 0xbe06fadb9f155744
